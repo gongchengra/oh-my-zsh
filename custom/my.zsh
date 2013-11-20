2d15232a34beddd5d3f82c5f17e64e051dee896b
Cheng Gong <gongchengra@gmail.com>
2013-11-20 10:32:35 +0800

    myown

---
+++ custom/my.zsh
@@ -0,0 +1,14 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+alias la='ls -A'
+alias go='git checkout'
+alias gs='git status'
+alias gc='git commit -am'
+alias gb='git branch'
+alias gp='git push'
+alias gpl='git pull'
+
+
