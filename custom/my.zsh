39e5d0f1f09b7ee922119c64bfd01f965bdd68ab
alan <gongchengra@gmail.com>
2014-04-14 01:43:51 -0400

    changed customer setting

--- custom/my.zsh
+++ custom/my.zsh
@@ -4,15 +4,18 @@
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
 alias la='ls -A'
-alias go='git checkout'
+alias go='sudo git checkout'
 alias gs='git status'
 alias gc='git commit -am'
 alias gb='git branch'
-alias gp='git push'
-alias gpl='git pull'
+alias gp='sudo git push'
+alias gpl='sudo git pull'
 alias gv='git remote -v'
 alias fn='find . -name'
 alias pg='ps aux|grep'
 alias rf='rm -rf'
 alias ll='ls -laSrh'
-
+alias je='cd /data/content/errors'
+alias ji='cd /data/content/ID-BUILDS/'
+alias jt='cd /tmp/tomcat7-tomcat7-tmp/'
+alias jc='cd /var/www/pharoscontent/cocoon/'
